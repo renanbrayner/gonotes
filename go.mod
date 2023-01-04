d4b8cd873725b02d99a33e73bf23fec01dcf1228
Renan Brayner <renanbrayner@gmail.com>
2023-01-04 14:01:14 -0300

    basic functionality (append if it has args, open editor if it doesn't)

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module notes
 
 go 1.19
 
-require (
-	github.com/pelletier/go-toml v1.9.5 // indirect
-	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
-)
+require github.com/pelletier/go-toml/v2 v2.0.6
--- main.go
+++ main.go
@@ -1,111 +1,142 @@
 package main
 
 import (
-  "os"
-  "os/exec"
-  "fmt"
-  "time"
-  "io/ioutil"
-  "github.com/pelletier/go-toml/v2"
+	"fmt"
+	"github.com/pelletier/go-toml/v2"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
 )
 
+// TODO: lidar com configuração vazia
 type Config struct {
-  Editor string
-  Directory_name string
-  Filetype string
+	Editor         string
+	Directory_name string
+	Filetype       string
 }
 
 var cfg Config
 
 func main() {
-  errReadConfig := readConfig()
-  if errReadConfig != nil {
-    panic(errReadConfig)
-  }
+	errReadConfig := readConfig()
+	if errReadConfig != nil {
+		panic(errReadConfig)
+	}
 
-  errCreateNote := createNoteFile()
-  if errCreateNote != nil {
-    panic(errReadConfig)
-  }
+	if fileExists(fmt.Sprintf("%s/%s", getNoteFileDirectory(), getNoteFileName())) == false {
+		errCreateNoteFile := createFileRecursive(getNoteFileDirectory(), getNoteFileName())
+		if errCreateNoteFile != nil {
+			panic(errCreateNoteFile)
+		}
+	}
+
+	args := os.Args[1:]
+
+	if len(args) == 0 {
+		openNoteFileInEditor()
+	} else {
+		file, err := os.OpenFile(
+			fmt.Sprintf("%s/%s", getNoteFileDirectory(), getNoteFileName()),
+			os.O_APPEND|os.O_WRONLY|os.O_CREATE,
+			0600,
+		)
+		if err != nil {
+			panic("Unable to open file")
+		}
+		defer file.Close()
+
+		if _, err = file.WriteString(strings.Join(args, " ") + "\n\n"); err != nil {
+			panic(err)
+		}
+	}
 }
 
-// TODO: criar função de conseguir nome do arquivo e caminho do arquivo para poder criar e ler
-// TODO: ele de forma mais fácil
-// func getNoteFileName() string {
-//   now := time.Now()
-// }
+func openNoteFileInEditor() {
+	editorCmd := exec.Command(
+		"nvim",
+		fmt.Sprintf("%s/%s", getNoteFileDirectory(), getNoteFileName()),
+	)
+	editorCmd.Stdin = os.Stdin
+	editorCmd.Stdout = os.Stdout
+	editorCmd.Stderr = os.Stderr
 
-func createNoteFile() error {
-  now := time.Now()
+	errStart := editorCmd.Start()
+	if errStart != nil {
+		fmt.Println(errStart)
+	}
 
-  home := os.Getenv("HOME")
-  if home == "" {
-    return fmt.Errorf("error reading XDG_CONFIG_HOME")
-  }
+	errWait := editorCmd.Wait()
+	if errWait != nil {
+		fmt.Println(errWait)
+	}
+}
 
-  year := now.Year()
-  month := fmt.Sprintf("%02d", now.Month())
-  day := fmt.Sprintf("%02d", now.Day())
+func getNoteFileName() string {
+	now := time.Now()
+	return fmt.Sprintf("%02d%s", now.Day(), cfg.Filetype)
+}
 
-  createFileRecursive(
-    fmt.Sprintf("%s/%s/%d/%s", home, cfg.Directory_name, year, month),
-    fmt.Sprintf("%s%s", day, cfg.Filetype),
-  )
-
-  return nil
+func getNoteFileDirectory() string {
+	now := time.Now()
+	home := os.Getenv("HOME")
+	if home == "" {
+		panic("Error reading HOME")
+	}
+	return fmt.Sprintf("%s/%s/%d/%02d", home, cfg.Directory_name, now.Year(), now.Month())
 }
 
 func readConfig() error {
-  // Read config home
-  configHome := os.Getenv("XDG_CONFIG_HOME")
-  if configHome == "" {
-    return fmt.Errorf("error reading XDG_CONFIG_HOME")
-  }
+	// Read config home
+	configHome := os.Getenv("XDG_CONFIG_HOME")
+	if configHome == "" {
+		return fmt.Errorf("error reading XDG_CONFIG_HOME")
+	}
 
-  // Check if config file exists and create one if it doesn't
-  if fileExists(configHome + "/notes/config.toml") == false {
-    err := createFileRecursive(configHome + "/notes", "config.toml")
-    if err != nil {
-      panic(err)
-    }
-  }
+	// Check if config file exists and create one if it doesn't
+	if fileExists(configHome+"/notes/config.toml") == false {
+		err := createFileRecursive(configHome+"/notes", "config.toml")
+		if err != nil {
+			panic(err)
+		}
+	}
 
-  // Read file
-  tomlData, errRead := ioutil.ReadFile(configHome + "/notes/config.toml")
-  if errRead != nil {
-    panic("Error reading config file")
-  }
+	// Read file
+	tomlData, errRead := ioutil.ReadFile(configHome + "/notes/config.toml")
+	if errRead != nil {
+		panic("Error reading config file")
+	}
 
-  // Save it as struct to cfg variable
-  err := toml.Unmarshal(tomlData, &cfg)
-  if err != nil {
-    return err
-  }
+	// Save it as struct to cfg variable
+	err := toml.Unmarshal(tomlData, &cfg)
+	if err != nil {
+		return err
+	}
 
-  return nil
+	return nil
 }
 
 func fileExists(path string) bool {
-  _, err := os.Stat(path)
-  if os.IsNotExist(err) {
-    return false
-  } else if err != nil {
-    panic("Error checking if file " + path + " exists")
-  }
-  return true
+	_, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		return false
+	} else if err != nil {
+		panic("Error checking if file " + path + " exists")
+	}
+	return true
 }
 
 func createFileRecursive(dirPath string, fileName string) error {
-  fmt.Printf("Creating file %s/%s\n", dirPath, fileName)
-  errDir := os.MkdirAll(dirPath, 0777)
-  if errDir != nil {
-    return errDir
-  }
+	errDir := os.MkdirAll(dirPath, 0777)
+	if errDir != nil {
+		return errDir
+	}
 
-  file, errFile := os.Create(fmt.Sprintf("%s/%s", dirPath, fileName))
-  if errFile != nil {
-    return errFile
-  }
-  defer file.Close()
-  return nil
+	file, errFile := os.Create(fmt.Sprintf("%s/%s", dirPath, fileName))
+	if errFile != nil {
+		return errFile
+	}
+	defer file.Close()
+	return nil
 }
